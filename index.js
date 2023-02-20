ce0ba7729e9308ee7f194d0103a2f988584df5ec
Jun <numsil8567@gmail.com>
2023-02-20 23:50:26 +0900

    feat : foreach , map, fill study

--- index.js
+++ index.js
@@ -119,20 +119,100 @@ const getlocation = (c, x, y) => {
 
 getlocation("C");
 
-let arr = [1, 2, 3, 4, 5];
-let result = arr.splice(1, 2);
-console.log(arr);
-console.log(result);
-let plusarry = arr.concat([7, 8]);
-console.log(plusarry);
+// let arr = [1, 2, 3, 4, 5];
+// let result = arr.splice(1, 2);
+// console.log(arr);
+// console.log(result);
+// let plusarry = arr.concat([7, 8]);
+// console.log(plusarry);
 
-let users = ["a", "b", "c"];
+let users = ["n", "b", "a"];
 users.forEach((item, index, arr) => {
   console.log(`${index}, ${item}, ${arr}`);
 });
+
+function solution(common) {
+  const length = common.length;
+  const diff = common[1] - common[0]; // 등차수열의 공차
+  const ratio = common[1] / common[0]; // 등비수열의 공비
+  console.log("common", diff);
+
+  if (common[length - 1] - common[length - 2] === diff) {
+    // 주어진 수열이 등차수열인 경우
+    return common[length - 1] + diff;
+  } else {
+    // 주어진 수열이 등비수열인 경우
+    return common[length - 1] * ratio;
+  }
+}
 
 let arryItem = [1, 2, 3, 4, 5];
 let find = arryItem.filter((item) => {
   return item % 2 === 0;
 });
 console.log("find", find);
+
+// function solution(num, total) {
+//   let start  Math.floor(total / num) - Math.floor(num / 2);
+//   const result = [];
+
+//   if (start <= 0) {
+//     start = 1;
+//   }
+
+//   for (let i = 0; i < num; i++) {
+//     result.push(start + i);
+//   }
+
+//   if (result.reduce((acc, cur) => acc + cur, 0) !== total) {
+//     return [-1];
+//   }
+
+//   return result;
+// }
+// console.log("cote", solution(4, 14));
+
+function solution(num, total) {
+  const start = Math.floor((total - (num * (num - 1)) / 2) / num);
+  if (start < 1) {
+    return [-1];
+  }
+  return Array.from({ length: num }, (_, i) => start + i);
+}
+console.log(solution(5, 1150));
+
+const answer = [3, 1, 4, 6];
+const value = "3214";
+let strike = 0;
+let ball = 0;
+answer.forEach((element, index) => {
+  console.log("value", element === 3);
+});
+for (let i = 0; i < answer.length; i++) {
+  const index = value.indexOf(answer[i]);
+  if (index > -1) {
+    if (index === i) {
+      strike += 1;
+    } else {
+      ball += 1;
+    }
+  }
+}
+console.log(strike);
+
+const array = [1, 2, 3, 4];
+const result = [];
+// for (let i = 0; i < 4; i++) {
+//   result.push(array[i] * 2);
+// }
+array.map((element, i) => {
+  return element * 2;
+});
+
+const a = Array(5)
+  .fill(0)
+  .map((e, i) => {
+    return i + 1;
+  });
+
+console.log(a);
